344d1c8c586093d64ab690a7217acb6c370d0390
Stefano Crespi <stefano.crespi@lexisnexis.com>
2015-03-31 13:57:20 -0500

    Minor change to radix sort.

--- src/main/java/sort/Radix.java
+++ src/main/java/sort/Radix.java
@@ -16,23 +16,30 @@ public final class Radix {
                 m = a[i];
         while (m / exp > 0)
         {
+            System.out.println("radix: " + exp);
             int[] bucket = new int[10];
 
             for (i = 0; i < n; i++)
                 bucket[(a[i] / exp) % 10]++;
+            System.out.println("bucket count");
+            System.out.println(Arrays.toString(bucket));
             for (i = 1; i < 10; i++)
                 bucket[i] += bucket[i - 1];
+            System.out.println("bucket accumulator");
+            System.out.println(Arrays.toString(bucket));
             for (i = n - 1; i >= 0; i--)
                 b[--bucket[(a[i] / exp) % 10]] = a[i];
+            System.out.println("b " + Arrays.toString(b));
             for (i = 0; i < n; i++)
                 a[i] = b[i];
+            System.out.println("a " + Arrays.toString(a));
             exp *= 10;
         }
     }
 
     public static void main(String... args) {
         // input > 0
-        int[] array = {4,5,2,7,3,6,6,8,9,1};
+        int[] array = {14,25,12,27,13,26,16,29,29,11};
         System.out.println(Arrays.toString(array));
         sort(array);
         System.out.println(Arrays.toString(array));
